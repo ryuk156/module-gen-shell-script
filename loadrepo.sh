e8372f2f59b48ed2e9730194dfa17cbfea80ef72
ryuk156 <yp15601560@gmail.com>
2021-06-06 20:38:47 +0530

    fix: jenkins file

--- loadrepo.sh
+++ loadrepo.sh
@@ -2,11 +2,18 @@
 
 allRepos=()
 
-for i in {1..3}; do
+fetch(){
+    for i in {1..3}; do
     getRepos=$(curl -s "https://api.github.com/orgs/terasology/repos?q=&per_page=100&sort=name&page=$i")
     parseData=$(echo "$getRepos" | grep -oP '(?<="name": ")[^"]*')
     allRepos+=$parseData
-done
+    done
+
+    return $allRepos
+   
+}
+
+fetch
 
 for value in "${allRepos[@]}"; do
     echo "$value"
