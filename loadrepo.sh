e5ad20ea32c19e6eca528ed6054f4b3c487f7c75
ryuk156 <yp15601560@gmail.com>
2021-06-07 12:25:03 +0530

    fix: jenkins file

--- loadrepo.sh
+++ loadrepo.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-allRepos=()
+touch allmodules.txt
 
-fetch() {
-    for i in {1..3}; do
+for i in {1..3}; do
+
         getRepos=$(curl -s "https://api.github.com/orgs/terasology/repos?q=&per_page=100&sort=name&page=$i")
-        parseData=$(echo "$getRepos" | grep -oP '(?<="name": ")[^"]*')
-        allRepos+=("$parseData")
-    done
-    return $allRepos
-}
+        parseData=$(echo "$getRepos" | grep -oP '(?<="full_name": "Terasology/)[^"]*')
+        if [ -s allmodules.txt ]; then
+                echo "$parseData" >>allmodules.txt
+        else
+                echo "$parseData" >allmodules.txt
+        fi
+done
 
-fetch
+readarray allrepos < allmodules.txt
 
-# for value in "${allRepos[@]}"; do
-#     echo "$value"
-# done
+for row in "${allrepos[@]}"; do
+        echo "$row"
+done
 
 mkdir meta-data
 mkdir clonedrepos
 
 cd clonedrepos
 
-for value in "${allRepos[@]}"; do
-    git clone "https://github.com/Terasology/${value}"
-    cd "$value"
-    ../../scrape.sh
-    cd ..
+for value in "${allrepos[@]}"; do
+
+        git clone "https://github.com/Terasology/${value}"
+        cd "${value}"
+        ../../scrape.sh
+        cd ..
 done
