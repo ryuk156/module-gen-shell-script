7c576493b99813eda492e2615bedc5952932dcdd
ryuk156 <yp15601560@gmail.com>
2021-06-07 14:22:09 +0530

    fix: jenkins file

--- loadrepo.sh
+++ loadrepo.sh
@@ -18,17 +18,32 @@ readarray allrepos < allmodules.txt
 #         echo "${row}" | grep -v "^$" 
 # done
 
-mkdir meta-data
+
 mkdir clonedrepos
 
 cd clonedrepos
 
+
 for value in "${allrepos[@]}"; do
+#        cd ../meta-data
+#        mkdir ${value//$'\n'/}
+
+#        cd ${value//$'\n'/}
+#        touch module.txt
+#        touch readme.txt
+#        pwd
+#        cd ..
 
         # cleanvalue=$(echo "${value}" | grep -v "^$")
         # echo "${cleanvalue}"
         git clone "https://github.com/Terasology/${value//$'\n'/}"
         cd "${value//$'\n'/}"
-        ../../scrape.sh
         cd ..
+done
+
+
+for d in * ; do
+    cd $d
+    ../../scrape.sh
+    cd ..
 done
