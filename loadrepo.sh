37b59960bf819f3ed3ff24d27ebd03d40e7adb54
ryuk156 <yp15601560@gmail.com>
2021-06-07 12:50:29 +0530

    fix: jenkins file

--- loadrepo.sh
+++ loadrepo.sh
@@ -2,8 +2,8 @@
 
 touch allmodules.txt
 
-for i in {1..2}; do
-        getRepos=$(curl -s "https://api.github.com/orgs/terasology/repos?q=&per_page=1&sort=name&page=$i")
+for i in {1..3}; do
+        getRepos=$(curl -s "https://api.github.com/orgs/terasology/repos?q=&per_page=100&sort=name&page=$i")
         parseData=$(echo "$getRepos" | grep -oP '(?<="full_name": "Terasology/)[^"]*')
         if [ -s allmodules.txt ]; then
                 echo "$parseData" >>allmodules.txt
