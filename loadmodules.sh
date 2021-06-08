bb7625e4a09713aa75746a5bc1622353578d1d7b
ryuk156 <yp15601560@gmail.com>
2021-06-08 18:38:47 +0530

    add: modulesite script

--- loadmodules.sh
+++ loadmodules.sh
@@ -1,10 +1,11 @@
 MODULEDIR="./module-site"
 MODULES="./modules"
+DATE=$(date)
 
 mkdir $MODULEDIR
 cd $MODULEDIR
 
-git clone https://github.com/MovingBlocks/ModuleSite.git
+git clone https://github.com/ryuk156/ModuleSite.git
 
 cd ModuleSite
 
@@ -15,6 +16,21 @@ cd ..
 cd ..
 
 cp -r $MODULES  ./module-site/ModuleSite
+
+cd ./module-site/ModuleSite
+
+
+git add .
+
+git commit -m "Modules push on $DATE"
+
+git remote add upstream https://github.com/ryuk156/ModuleSite.git
+
+git push origin build_process
+
+
+
+
 
 
 
