a65d02be8aee04c6a85d3c415dcd825cac33ac56
ryuk156 <yp15601560@gmail.com>
2021-06-09 13:31:37 +0530

    add: modulesite script

--- openpr.sh
+++ openpr.sh
@@ -16,9 +16,7 @@ data=$(
 END
 )
 
-if git diff-index --quiet HEAD --; then
-    echo "Nothing to psuh"
-else
-    git push https://${GIT_CREDS_USR}:${GIT_CREDS_PSW}@github.com/${GIT_CREDS_USR}/ModuleSite.git module_gen -f
-    curl -i -H "Authorization: token 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" -X POST "https://api.github.com/repos/ryuk156/ModuleSite/pulls" -d "$data"
-fi
+
+git push https://${GIT_CREDS_USR}:${GIT_CREDS_PSW}@github.com/${GIT_CREDS_USR}/ModuleSite.git module_gen -f
+curl -i -H "Authorization: token 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" -X POST "https://api.github.com/repos/ryuk156/ModuleSite/pulls" -d "$data"
+
